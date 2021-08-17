0594e84866409a66bda0fbce0ac7d3eef102f042
washington-shoji <shoji.washington@gmail.com>
2021-08-17 17:07:09 +1000

    Refactored the MyApp component with fontawesome config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
-import '../styles/globals.css';
 import '@fortawesome/fontawesome-svg-core/styles.css'; // import Font Awesome CSS
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {fab} from '@fortawesome/free-brands-svg-icons';
 import {config} from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
+import '../styles/globals.css';
 import type {AppProps} from 'next/app';
-import Layout from '../components/Layout/Layout';
+import Layout from '../components/layout/Layout';
+
+library.add(fab); // Add access to fontawesome brand icons to entire app
 
 function MyApp({Component, pageProps}: AppProps) {
 	return (
